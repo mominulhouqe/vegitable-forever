f2113f67a89640f1ec8e7bb55d3b46e72b101ba3
mominul <ulhouqemomin39@gmail.com>
2023-06-18 14:25:24 +0600

    added apps route setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,15 @@
+import Main from "../Layout/Main";
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import Home from "../pages/Home/Home/Home";
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />,
+        children:[{
+            path:'/',
+            element: <Home />
+        }]
+    }
+])
