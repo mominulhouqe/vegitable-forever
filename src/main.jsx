593757dc59553818df1ce7c50cd975b22774eda6
mominul <ulhouqemomin39@gmail.com>
2023-09-07 12:02:21 +0600

    added asset

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { router } from './Routes/Routes.jsx'
+import { RouterProvider } from 'react-router-dom'
 
 
 
